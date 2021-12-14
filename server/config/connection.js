023460ded27d25ee8218759e0cc2dafbf5d69bb7
Maki Aboabida <maki-miko@hotmail.com>
2021-12-14 00:03:25 -0500

    Added: scripts to package.json for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/deep-thoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  // useCreateIndex: true
 });
 
 module.exports = mongoose.connection;
